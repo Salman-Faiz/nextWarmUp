913f820ea3296a9ff9456aea721b2036801b5a35
Salman-Faiz <salmanfaiz.cse@gmail.com>
2025-04-14 01:38:00 +0600

    page added

---
+++ app/dashboard/settings/page.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const page = () => {
+     return (
+          <div className='lg:flex justify-center h-36 items-center mx-10 my-4 border-2 border-amber-500  gap-3'>
+               {/* <div className='bg-red-400'>1</div>
+               <div>2</div>
+               <div>3</div>
+               <div >4</div>
+               <div>5   </div>
+               <div className='bg-red-400 col-span-2'>6</div>
+               <div >7</div>
+               <div>8</div>
+               <div>9</div>
+               <div >10</div>
+               <div>11</div> */}
+              <div className='text-center'>hello</div>
+              <div>world</div>
+           
+              
+               
+          </div>
+     );
+};
+
+export default page;
